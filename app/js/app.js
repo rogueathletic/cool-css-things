2f50ad9482e09930db8e3db23079185e5e7413ef
Jason Schutz <jason@rogueathletic.com>
2019-03-13 00:24:19 -0700

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
     $('div').mouseover(function(){
       $(this).addClass($(this).data('class')+' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-        $(this).removeClass()
+        $(this).removeClass();
       });
-    })
-  })
-  
+    });
+  });
   
